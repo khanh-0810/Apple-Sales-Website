c0dca41002144003f30841d55d441c16de01d376
NhutKhanh0810 <114718990+khanh-0810@users.noreply.github.com>
2024-11-05 11:33:30 +0700

    Update components.js

--- js/components.js
+++ js/components.js
@@ -33,7 +33,7 @@ function getProductCard(productFeatured) {
                         <h3>${productFeatured.id}</h3>
                     </div>
                     <div class="col-12">
-                        <a href="./pages/auth/Product-detail.html?id=${productFeatured.id}">See Details</a>
+                        <a href="./pages/auth/Product-detail.html?id=${productFeatured.id}">See etails</a>
                     </div>
                 </div>
             </div>
